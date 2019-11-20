982932b938cd87143d223d7d0928bd2b92231792
Mike Matera <maximus@fatboycentral.com>
2019-11-20 08:56:35 -0800

    Install fonts in setup.

--- setup.sh
+++ setup.sh
@@ -23,3 +23,17 @@ for config in $scripthome/config/*; do
     echo "Linking $configtarget -> $config"
     ln -fs $config $configtarget 
 done
+
+# Install fonts
+echo "Installing fonts."
+(
+    if [ ! -d $HOME/.local/share/fonts ]; then 
+	mkdir -p $HOME/.local/share/fonts
+    fi
+    cd $HOME/.local/share/fonts 
+    wget https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Regular.ttf
+    wget https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Bold.ttf
+    wget https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Italic.ttf
+    wget https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Bold%20Italic.ttf
+)
+fc-cache -fv
